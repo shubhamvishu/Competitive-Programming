50184b69d97027d986aa2399acf573be6daaadae
Shubham Chaudhary <shubhmasand@gmail.com>
2021-02-06 13:08:56 +0530

    Binary search

---
+++ Algorithms/Searching/BinarySearch.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/binary-search/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start=0, end=n-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target) start=mid+1;
+            else end=mid-1;
+        }
+        return -1;
+    }
+};
