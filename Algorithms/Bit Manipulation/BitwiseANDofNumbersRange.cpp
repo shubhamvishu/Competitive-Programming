e4a4604a6cca6a482592ab530e3dddfabebb7dee
Shubham Chaudhary <shubhmasand@gmail.com>
2021-10-19 22:20:32 +0530

    201. Bitwise AND of Numbers Range

---
+++ Algorithms/Bit Manipulation/BitwiseANDofNumbersRange.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/bitwise-and-of-numbers-range/
+
+class Solution {
+public:
+    int min(int a, int b){
+        return a<b?a:b;
+    }
+    int rangeBitwiseAnd(int left, int right) {
+        if(left==0) return 0;
+        int ans = 0,temp = left, lcnt = 0,rcnt = 0,max_right = -1+pow(2,31);
+        while(temp){
+            lcnt++;
+            temp>>=1;
+        }
+        temp = right;
+        while(temp){
+            rcnt++;
+            temp>>=1;
+        }
+        if(lcnt!=rcnt) return 0;
+        else{
+                if(rcnt==31) max_right = pow(2,31)-1;
+                else max_right = 1<<rcnt;
+                for(int i=left; i<=min(right,max_right); i++){
+                    if(i==left) ans = left;
+                    else ans&=i;
+                    if(!ans) break;
+                    if(i==right) break;
+                }
+        }
+
+        return ans;
+    }
+};
